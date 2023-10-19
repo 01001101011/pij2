99cb23db987d317d6aab9e00dc93c045a5b26b1c
01001101011 <pmoura02@student.bbk.ac.uk>
2023-10-19 22:12:22 +0100

    final_draft

--- src/d5e11.java
+++ src/d5e11.java
@@ -28,42 +28,43 @@ public class d5e11 {
     }
 
     public static void main(String[] args) {
-        String[] name = {"a", "b", "c"};
-        String y = "d";
-        int[] id = {1, 2, 3};
-        int x = 9;
-        name = addStr(name, y);
-        id = addInt(id, x);
-        for (int i = 0; i < 4; i++) {
-            System.out.println(name[i]);
-            System.out.println(id[i]);
+        Scanner scanStr = new Scanner(System.in);
+        Scanner scanInt = new Scanner(System.in);
+        String[] name = new String[10];
+        int[] id = new int[10];
+        String dummyName;
+        int dummyId;
+        int lenArray = id.length;
+        int i = 0;
+        boolean f = false;
+        do {
+            System.out.println("Please enter name: ");
+            dummyName = scanStr.nextLine();
+            System.out.println("Please enter ID number:");
+            dummyId = scanInt.nextInt();
+            int lenNameInput = dummyName.length();
+            //if ("".equals(lenNameInput)) break;
+            if ("".equals(dummyName)) break;
+            if (dummyId == 0) break;
+            if (i < lenArray) {
+                name[i] = dummyName;
+                id[i] = dummyId;
+            } else
+            if (i == lenArray) {
+                name = addStr(name, dummyName);
+                id = addInt(id, dummyId);
+                lenArray++;
+            }
+            i++;
+        } while (true);
+
+        for (int j = 0; j < id.length; j++) {
+            if ("S".equals(name[j].substring(0, 1))) {
+                System.out.println("Name: " + name[j] + ", ID:" + id[j]);
+            }
+            if (id[j] % 2 == 0) {
+                System.out.println("Name: " + name[j] + ", ID:" + id[j]);
+            }
         }
-
     }
-
-//    public static void main(String[] args) {
-//        Scanner scanStr = new Scanner(System.in);
-//        Scanner scanInt = new Scanner(System.in);
-//        String[] name = new String[10];
-//        int[] id = new int[10];
-//
-//        boolean f = false;
-//        int i = 0;
-//
-//        do {
-//            System.out.println("please enter name: ");
-//            name[i] = scanStr.nextLine();
-//            System.out.println("PLEASE ENTER ID:");
-//            id[i] = scanInt.nextInt();
-//
-//            //f = " ".equals(name[i]);
-//            int len = name[i].length();
-//            if (len == 0) f = true;
-//            i++;
-//        } while (f == false);
-//        for (int i=0; i < 10; i++) {
-//            System.out.println(name[i]);
-//            System.out.println(id[i]);
-//        }
-//    }
 }
