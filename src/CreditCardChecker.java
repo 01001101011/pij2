944fbe557ca1f57356f1d9a7ae1ff583cdc6e5b3
01001101011 <pmoura02@student.bbk.ac.uk>
2023-10-21 18:32:44 +0100

    a

--- src/CreditCardChecker.java
+++ src/CreditCardChecker.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
-public class CreditCardChecker {
-    int digit;
-    boolean pos;
-
-    public int decode(int digit, boolean pos) {
-        
-    }
-
-
-}
+//import java.util.Scanner;
+//public class CreditCardChecker {
+//    int digit;
+//    boolean pos;
+//
+//    public int decode(int digit, boolean pos) {
+//
+//    }
+//
+//
+//}
